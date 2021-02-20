dbd1a817434b8a14497333dcd2beb3d9d41bc341
syam <syam@securityadvisor.io>
2021-02-20 18:17:44 +0530

    cache

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,5 +1,5 @@
 import {
-  Body,
+  Body, CacheInterceptor,
   Controller,
   Delete,
   Get,
@@ -9,13 +9,14 @@ import {
   Put, Query,
   Req,
   Request,
-  UseGuards,
+  UseGuards, UseInterceptors,
 } from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto, ReviewFilterDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Review } from './entities/reviews.entity';
+
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'))
@@ -25,19 +26,21 @@ export class ReviewsController {
   private logger = new Logger('Review Controller');
   constructor(private reviewService: ReviewsService) {}
 
+  @UseInterceptors(CacheInterceptor)
   @Get('reviews-stats')
   getStats(@Req() req: any): Promise<any> {
     const userId = req.user.id;
-    this.logger.verbose('Api Triggered', req.user.email);
+    this.logger.verbose('reviews-stats Api Triggered', req.user.email);
     return this.reviewService.getStats(userId);
   }
 
+  @UseInterceptors(CacheInterceptor)
   @Get('all-reviews')
   getAllReviews(
     @Query() reviewFilterDto: ReviewFilterDto,
     @Req() req: any,
   ): Promise<any> {
-    this.logger.verbose('Api Triggered', req.user.email);
+    this.logger.verbose('all-reviews Api Triggered', req.user.email);
     return this.reviewService.getAllReviews(reviewFilterDto);
   }
 
@@ -48,13 +51,13 @@ export class ReviewsController {
   ): Promise<Review> {
     createReviewDto.status = 'pending';
     createReviewDto.userId = req.user.id;
-    this.logger.verbose('Api Triggered', req.user.email);
+    this.logger.verbose('create-review Api Triggered', req.user.email);
     return this.reviewService.createReview(createReviewDto);
   }
 
   @Get('get-review/:id')
   getReviewById(@Param('id') id: number, @Req() req: any): Promise<Review> {
-    this.logger.verbose('Api Triggered', req.user.email);
+    this.logger.verbose('get-review/:id Api Triggered', req.user.email);
     return this.reviewService.getReviewById(id);
   }
 
@@ -64,14 +67,14 @@ export class ReviewsController {
     @Body('status') status: string,
     @Request() req: any,
   ) {
-    this.logger.verbose('Api Triggered', req.user.email);
+    this.logger.verbose('update-status/:id Api Triggered', req.user.email);
     return this.reviewService.updateReviewStatus(id, status, req);
   }
 
   @Delete('delete/:id')
-  destroyReview(@Request() req: any, @Param('id') id: number) {
-    this.logger.verbose('Api Triggered', req.user.email);
+  async destroyReview(@Request() req: any, @Param('id') id: number) {
+    this.logger.verbose('delete/:id Api Triggered', req.user.email);
     this.logger.verbose(`Review with ${id} deleted`);
-    return this.reviewService.deleteReview(id);
+    return this.reviewService.deleteReview(id, req);
   }
 }
--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from "@nestjs/common";
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { Review } from './entities/reviews.entity';
@@ -6,9 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import Users from '../auth/entities/users.entity';
 import { UserRepository } from '../auth/user.repository';
 import { ReviewRepository } from './review.repository';
+import * as redisStore from 'cache-manager-redis-store';
+
 
 @Module({
   imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.X_REDIS_HOST,
+      port: process.env.X_REDIS_PORT,
+    }),
     TypeOrmModule.forFeature([Users, UserRepository]),
     TypeOrmModule.forFeature([Review, ReviewRepository]),
   ],
