71bdd7fd35ad91130282634dedba3675d1d42ceb
syam <syam@securityadvisor.io>
2021-02-20 16:53:24 +0530

    changes for swagger

--- src/reviews/dto/review-filter.dto.ts
+++ src/reviews/dto/review-filter.dto.ts
@@ -2,43 +2,43 @@ import { IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ReviewFilterDto {
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null , required: false})
   @IsOptional()
   userId: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null , required: false})
   @IsOptional()
   destinationId: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null, required: false })
   @IsOptional()
   facilityId: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null, required: false })
   @IsOptional()
   activityId: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null , required: false})
   @IsOptional()
   typeId: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null , required: false})
   @IsOptional()
   status: string;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null , required: false})
   @IsOptional()
   page: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null , required: false})
   @IsOptional()
   offset: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null, required: false })
   @IsOptional()
   limit: number;
 
-  @ApiProperty({ example: null })
+  @ApiProperty({ example: null, required: false })
   @IsOptional()
   date: Date;
 }
