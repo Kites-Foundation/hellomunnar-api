09862c0df5128f6180928c95c2af6820356fe028
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-02-27 11:30:44 +0530

    added try catch to googleLogin

--- src/auth/services/google.service.ts
+++ src/auth/services/google.service.ts
@@ -17,64 +17,72 @@ export class GoogleService {
   ) {}
 
   async googleLogin(token: String): Promise<any> {
-    const tokenVerifier = async (token) => {
-      const verify = async () => {
-        const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-        const client = new AuthValidator.OAuth2Client(CLIENT_ID);
-        const ticket = await client.verifyIdToken({
-          idToken: token,
-          audience: CLIENT_ID,
-        });
-        const payload = ticket.getPayload();
-        return payload;
+    try {
+      const tokenVerifier = async (token) => {
+        const verify = async () => {
+          const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+          const client = new AuthValidator.OAuth2Client(CLIENT_ID);
+          const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,
+          });
+          const payload = ticket.getPayload();
+          return payload;
+        };
+        return verify().catch(console.error);
       };
-      return verify().catch(console.error);
-    };
 
-    const result = await tokenVerifier(token);
-    if (result) {
-      const user = await this.userRepository.findOne({
-        email: result.email,
-      });
+      const result = await tokenVerifier(token);
+      if (result) {
+        const user = await this.userRepository.findOne({
+          email: result.email,
+        });
 
-      if (!user) {
-        const googleDto: any = {};
-        googleDto.name = result.name;
-        googleDto.email = result.email;
-        googleDto.password = await bcrypt.hash(token, 10);
-        googleDto.googleImageUrl = result.picture;
-        googleDto.uuid = uuidv4();
-        googleDto.status = 'ACTIVE';
-        googleDto.type = 'USER';
+        if (!user) {
+          const googleDto: any = {};
+          googleDto.name = result.name;
+          googleDto.email = result.email;
+          googleDto.password = await bcrypt.hash(token, 10);
+          googleDto.googleImageUrl = result.picture;
+          googleDto.uuid = uuidv4();
+          googleDto.status = 'ACTIVE';
+          googleDto.type = 'USER';
 
-        const saveUser = await this.userRepository.save(googleDto);
-        const { ...savedUser } = saveUser;
+          const saveUser = await this.userRepository.save(googleDto);
+          const { ...savedUser } = saveUser;
 
-        const { email, id } = saveUser;
-        const payload = { email, id };
-        delete savedUser.password;
-        delete savedUser.token;
+          const { email, id } = saveUser;
+          const payload = { email, id };
+          delete savedUser.password;
+          delete savedUser.token;
 
-        return {
-          success: true,
-          status: 200,
-          access_token: this.jwtService.sign(payload),
-          user: savedUser,
-        };
-      } else {
-        const { email, id } = user;
-        user.googleImageUrl = result.picture;
-        await this.userRepository.save(user);
-        const payload = { email, id };
-        delete user.password;
-        delete user.token;
-        return {
-          success: true,
-          status: 200,
-          access_token: await this.jwtService.sign(payload),
-          user: user,
-        };
+          return {
+            success: true,
+            status: 200,
+            access_token: this.jwtService.sign(payload),
+            user: savedUser,
+          };
+        } else {
+          const { email, id } = user;
+          user.googleImageUrl = result.picture;
+          await this.userRepository.save(user);
+          const payload = { email, id };
+          delete user.password;
+          delete user.token;
+          return {
+            success: true,
+            status: 200,
+            access_token: await this.jwtService.sign(payload),
+            user: user,
+          };
+        }
       }
+    } catch (err) {
+      this.logger.error(err);
+      return {
+        success: false,
+        message: 'Something went wrong..! Registration failed.',
+      };
     }
   }
 }
